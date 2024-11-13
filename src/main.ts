0c1c59e7bbce3ddb0280ff8bf2a286b1660d796e
hirakm0ventures <hirak@m0.ventures>
2024-11-13 21:57:04 +0530

    port details add

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3002);
+  const port = process.env.PORT ?? 3002;
+  await app.listen(port);
+  console.log('Port---', port);
 }
 bootstrap();
