3d4a39c09239f08da868d26e733080a1550470ea
hirakm0ventures <hirakroya50@gmail.com>
2024-11-17 01:56:46 +0530

    add cors and cookie parser

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //CROS policy
+  app.enableCors({
+    origin: 'http://localhost:3000', // Allow requests from your frontend's origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
+    allowedHeaders: 'Content-Type, Accept', // Allowed headers
+    credentials: true, // Allow credentials (cookies)
+  });
+  app.use(cookieParser()); // Enable cookie parsing
   const port = process.env.PORT ?? 3002;
   await app.listen(port);
   console.log('Port---', port);
