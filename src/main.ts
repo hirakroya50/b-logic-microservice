c56e0e1a2f9fd81e60b2227dbad5b34001320357
hirakroya50 <hirakroya50@gmail.com>
2025-01-02 14:00:53 +0530

    add cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //CROS policy
   app.enableCors({
-    origin: 'http://localhost:3000', // Allow requests from your frontend's origin
+    origin: ['http://localhost:3000', 'http://localhost:5173'], // Allow requests from your frontend's origin
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
     allowedHeaders: 'Content-Type, Accept', // Allowed headers
     credentials: true, // Allow credentials (cookies)
