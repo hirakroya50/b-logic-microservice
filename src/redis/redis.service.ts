c617621cd2443fc2b484650219140820a7577715
hirakroya50 <hirakroya50@gmail.com>
2025-01-05 14:23:32 +0530

    websocket code update

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -7,7 +7,6 @@ export class RedisService {
   async startSubscriber(onMessage: (channel: string, message: string) => void) {
     try {
       await this.redisClient.subscribe(process.env.REDIS_PUB_SUB_KEY);
-
       this.redisClient.on('message', (channel, message) => {
         console.log(`Received message from channel ${channel}:`, message);
         onMessage(channel, message);
--- src/websocket/websocket.module.ts
+++ src/websocket/websocket.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
-import { WebsocketService } from './websocket/websocket.service';
-import { RedisService } from 'src/redis/redis.service';
+import { WebsocketService } from './websocket.service';
 import { RedisModule } from 'src/redis/redis.module';
 
 @Module({
