10213ea1853675d30644fec7a48bcb7d843ba28b
hirakroya50 <hirakroya50@gmail.com>
2025-01-11 17:22:48 +0530

    update cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //CROS policy
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:5173'], // Allow requests from your frontend's origin
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:5173',
+      'https://d2yq29g6vw6zn5.cloudfront.net',
+    ], // Allow requests from your frontend's origin
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
     allowedHeaders: 'Content-Type, Accept', // Allowed headers
     credentials: true, // Allow credentials (cookies)
