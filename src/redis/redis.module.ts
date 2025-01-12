eca973a2916bebfd92b81251efdecac4d8c0d198
hirakroya50 <hirakroya50@gmail.com>
2025-01-12 21:20:13 +0530

    update env

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -8,12 +8,7 @@ import Redis from 'ioredis';
     {
       provide: 'REDIS_CLIENT',
       useFactory: () => {
-        const client = new Redis({
-          host: process.env.REDIS_HOST,
-          port: Number(process.env.REDIS_PORT),
-          username: process.env.REDIS_USERNAME,
-          password: process.env.REDIS_PASSWORD,
-        });
+        const client = new Redis(process.env.REDIS_URL);
         return client;
       },
     },
